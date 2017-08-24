f15807f506b33945c53ff13c71f0a6342d6282fe
Lei Qi <qileilove@gmail.com>
2017-08-24 09:39:20 +0800

    [QL] add swipe action

--- src/test/java/appium/untils/AppiumAction.java
+++ src/test/java/appium/untils/AppiumAction.java
@@ -1,6 +1,7 @@
 package appium.untils;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -38,6 +39,51 @@ public class AppiumAction extends LocateElement{
     public void typeElement(String locatorType, String locatorName, String input){
         waitElementToBeVisable(locatorType,locatorName);
         getElement(driver,locatorType,locatorName).sendKeys(input);
+    }
+
+    /**
+     * This Method for swipe up
+     *
+     */
+    public void swipeToUp() {
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.press(width / 2, height * 3 / 4).moveTo(width / 2, height / 4).release().perform();
+        // wait for page loading
+    }
+
+    /**
+     * This Method for swipe down
+     *
+
+     */
+    public void swipeToDown() {
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.press(width / 2, height / 4).moveTo(width / 2, height * 3 / 4).release().perform();
+        // wait for page loading
+    }
+
+    /**
+     * This Method for swipe Left
+     *
+     */
+    public void swipeToLeft( ) {
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.press(width * 3 / 4, height / 2).moveTo(width / 4, height / 2).release().perform();
+        // wait for page loading
+    }
+
+
+    public void swipeToRight() {
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.press(width / 4, height / 2).moveTo(width * 3 / 4, height / 2).release().perform();
 
     }
 
