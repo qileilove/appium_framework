2efd6871c46728a3cc2a487c87678026340ce064
Lei Qi <qileilove@gmail.com>
2017-09-22 10:11:08 +0800

    [QL] refactor the monkeyrunner class

--- src/test/java/Utility/MonkeyRunner/MonkeyTools.java
+++ src/test/java/Utility/MonkeyRunner/MonkeyTools.java
@@ -2,14 +2,27 @@ package Utility.MonkeyRunner;
 
 import com.android.chimpchat.adb.AdbBackend;
 import com.android.chimpchat.core.IChimpDevice;
+import com.android.chimpchat.hierarchyviewer.HierarchyViewer;
 
 /**
  * Created by lqi on 20/09/2017.
  */
 
 
-public class MonkeyTest {
+public class MonkeyTools {
+    public static  HierarchyViewer hierarchyViewer = null;
+
+    public static IChimpDevice ConnentDevices() {
+        IChimpDevice device = new AdbBackend().waitForConnection();
+        return  device;
+
+    }
+    public void TakeSreenShot(IChimpDevice device ,String fileName){
+        device.takeSnapshot().writeToFile(fileName, null);
+    }
+
     public static void main(String[] args) {
+
         // sdk/platform-tools has to be in PATH env variable in order to find adb
         IChimpDevice device = new AdbBackend().waitForConnection();
 
@@ -18,7 +31,7 @@ public class MonkeyTest {
 
         // Take a snapshot and save to out.png
         device.takeSnapshot().writeToFile("out.png", null);
-
+        hierarchyViewer.findViewById ( "dd" );
         device.dispose();
     }
 }
